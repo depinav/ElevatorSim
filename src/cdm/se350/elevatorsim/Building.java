8de89918aeeae95297a47b26b99e7bb52f74c9a0
Edric <Edsemailaddress@gmail.com>
2013-05-12 21:48:03 -0500

    Added exceptions and Junit Testing to building

--- src/cdm/se350/elevatorsim/Building.java
+++ src/cdm/se350/elevatorsim/Building.java
@@ -5,17 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 import cdm.se350.elevatorsim.elevator.ElevatorImpl;
 
-/**
- * 
- * A building class that holds a custom number of floors and elevators
- * 
- * @author 		Victor DePina
- * @author 		Edric Delleola
- * @since 		Version 1.0
- * 
- */
 
 public class Building {
 	
@@ -24,14 +19,19 @@ public class Building {
 	private DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	private Date date = new Date();
 	
-	/**
-	 * Creates a new building with a specified number of floors and elevators.
-	 * 
-	 * @param 		floors			The int representing the number of floors the building will hold.
-	 * @param 		elevators		The int representing the number of elevators in the building.
-	 * 
-	 */
 	public Building (int floors, int elevators){
+		
+		try{
+			if (floors <= 0) throw new Exception("Floors requires a number greater than 0: " + floors);
+		}catch (Exception flrError){
+			System.out.println("Error: " + flrError.getMessage());
+		}
+		
+		try{
+			if (elevators <= 0) throw new Exception("Elevators requires a number greater than 0: " + elevators);
+		}catch (Exception eleError){
+			System.out.println("Error: " + eleError.getMessage());
+		}
 		
 		for (int i = 0; i < floors; i++){
 			  floorList.add(new Floor());
@@ -44,24 +44,69 @@ public class Building {
 		System.out.println(dateFormat.format(date) + "\tBuilding created with " + floors + " floors and " + elevators + " elevators");
 	}
 	
-	/**
-	 * Returns the collection of the elevator list of the building.
-	 * @return		The collection holding the list of elevators.
-	 */
 	public ArrayList<ElevatorImpl> getElevatorList() {
 		
 		return elevatorList;
 	}
 	
-	/**
-	 * Sets the scale that the system will use for time.
-	 * @param 		_scaled		The long representing the scaled time.
-	 */
+	public ArrayList<Floor> getFloorList() {
+		return floorList;
+	}
+	
 	public void setScale(long _scaled) {
 		
 		for (int i = 0; i < elevatorList.size(); i++)
 			elevatorList.get(i).setScaled(_scaled);
 		
 		System.out.println(dateFormat.format(date) + "\tScale set to " + _scaled + ":1");
+	}
+	
+	
+	public static class BuildingTest extends TestCase {
+		private Building building = null;
+		
+		public BuildingTest (String name) throws Exception {
+			super(name);
+		}
+		
+		protected void setUp() throws Exception{
+			System.out.println("Setup Building");
+			building = new Building (20,5);
+		}
+		
+		protected void tearDown(){
+			System.out.println("TearDown Building");
+			building = null;
+		}
+		
+		public static Test suite (){
+			return new TestSuite(BuildingTest.class);
+		}
+		
+		public void testBuildingElevatorList () throws Exception{
+			System.out.println("Testing Building Get and Set Methods");
+			
+			assertTrue("Value: " + building.getElevatorList().size(), (building.elevatorList.size()) == (5));
+			assertTrue("Value: " + building.getFloorList().size(), (building.floorList.size()) == (20));
+		}
+		
+		public void testBuildingConstructorErrorHandling(){
+			System.out.println("Test Building Constructor Error Handling");
+			
+			try{
+				Building buildingTest = new Building(-2,5);
+				fail("Allowed negative number of floors");
+			}catch (Exception e){
+				System.out.println("Okay. Found : " + e.getMessage());
+			}
+			
+			try{
+				Building buildingTest = new Building(2,-5);
+				fail("Allowed negative number of floors");
+			}catch (Exception e2){
+				System.out.println("Okay. Found : " + e2.getMessage());
+				return;
+			}
+		}
 	}
 }
